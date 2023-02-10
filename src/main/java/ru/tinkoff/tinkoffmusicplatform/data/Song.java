d7718df79be40122e49a46baeb0b27b140a003ae
ZhurkinN <zhurkin236@gmail.com>
2023-02-11 02:19:58 +0300

    add: entities

---
+++ src/main/java/ru/tinkoff/tinkoffmusicplatform/data/Song.java
@@ -0,0 +1,49 @@
+package ru.tinkoff.tinkoffmusicplatform.data;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+
+import java.sql.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "song")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Song {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private String genre;
+
+    @Column(nullable = false, name = "add_date")
+    private Date addDate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Song song = (Song) o;
+        return id != null && Objects.equals(id, song.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
