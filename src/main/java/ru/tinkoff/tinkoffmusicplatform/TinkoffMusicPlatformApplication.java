61b8dcce6b6f6500d739569ce8acbc8c01a273da
ZhurkinN <zhurkin236@gmail.com>
2023-02-10 12:49:12 +0300

    Initial commit

---
+++ src/main/java/ru/tinkoff/tinkoffmusicplatform/TinkoffMusicPlatformApplication.java
@@ -0,0 +1,13 @@
+package ru.tinkoff.tinkoffmusicplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkoffMusicPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinkoffMusicPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/tinkoff/tinkoffmusicplatform/TinkoffMusicPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tinkoff.tinkoffmusicplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinkoffMusicPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
