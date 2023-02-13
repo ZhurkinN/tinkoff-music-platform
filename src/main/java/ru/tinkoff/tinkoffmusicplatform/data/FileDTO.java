b551904a7d49d677470e66cde8e018ebd5eaa132
aleshkiyle <aleksey.rodin62@gmail.com>
2023-02-13 14:39:47 +0300

    implement minio configuration and fileDTO entity

---
+++ src/main/java/ru/tinkoff/tinkoffmusicplatform/data/FileDTO.java
@@ -0,0 +1,37 @@
+package ru.tinkoff.tinkoffmusicplatform.data;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class FileDTO implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 232836038145089522L;
+
+    private String title;
+
+    private String description;
+
+    @SuppressWarnings("java:S1948")
+    private MultipartFile file;
+
+    private String url;
+
+    private Long size;
+
+    private String filename;
+}
--- src/main/java/ru/tinkoff/tinkoffmusicplatform/repository/SongRepository.java
+++ src/main/java/ru/tinkoff/tinkoffmusicplatform/repository/SongRepository.java
@@ -1,7 +1,6 @@
 package ru.tinkoff.tinkoffmusicplatform.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.tinkoff.tinkoffmusicplatform.data.Song;
 
