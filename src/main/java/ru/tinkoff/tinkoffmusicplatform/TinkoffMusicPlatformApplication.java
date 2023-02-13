ab00172fb091c2ea331de40cfaa8e83f693421b0
ZhurkinN <zhurkin236@gmail.com>
2023-02-13 17:34:57 +0300

    merging branches

--- src/main/java/ru/tinkoff/tinkoffmusicplatform/TinkoffMusicPlatformApplication.java
+++ src/main/java/ru/tinkoff/tinkoffmusicplatform/TinkoffMusicPlatformApplication.java
@@ -1,11 +1,7 @@
 package ru.tinkoff.tinkoffmusicplatform;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import ru.tinkoff.tinkoffmusicplatform.data.Song;
-import ru.tinkoff.tinkoffmusicplatform.repository.SongRepository;
 
 @SpringBootApplication
 public class TinkoffMusicPlatformApplication {
--- src/main/java/ru/tinkoff/tinkoffmusicplatform/controller/SongController.java
+++ src/main/java/ru/tinkoff/tinkoffmusicplatform/controller/SongController.java
@@ -1,11 +1,9 @@
 package ru.tinkoff.tinkoffmusicplatform.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.tinkoff.tinkoffmusicplatform.data.Song;
 import ru.tinkoff.tinkoffmusicplatform.service.SongService;
 
@@ -21,8 +19,44 @@ public class SongController {
         return this.songService.getSongById(id);
     }
 
+// Этим вариантом можно упаковать возврат треков без сортировки и с сортировкой
+//    @GetMapping
+//    public ResponseEntity<Iterable<Song>> getSongs
+//            (@RequestParam(value = "sort", required = false, defaultValue = "false") boolean isSort) {
+//        return isSort ? this.songService.getAllSongsSortedByGenre() : this.songService.getAllSongs();
+//    }
+
+
     @GetMapping
     public ResponseEntity<Iterable<Song>> getSongs() {
         return this.songService.getAllSongs();
+    }
+
+    @GetMapping("/sorted")
+    public ResponseEntity<Iterable<Song>> getSongsSortedByGenre(){
+        return songService.getAllSongsSortedByGenre();
+    }
+
+    @GetMapping("/title/{title}")
+    public ResponseEntity<Iterable<Song>> getSongsByTitle(@PathVariable("title") String title){
+        return songService.getSongsByTitle(title);
+    }
+
+    @GetMapping("/author/{author}")
+    public ResponseEntity<Iterable<Song>> getSongsByAuthor(@PathVariable("author") String author){
+        return songService.getSongsByAuthor(author);
+    }
+
+    @GetMapping("/genre/{genre}")
+    public ResponseEntity<Iterable<Song>> getSongsByGenre(@PathVariable("genre") String genre){
+        return songService.getSongsByGenre(genre);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity save(@RequestBody Song song){
+
+        songService.save(song);
+
+        return ResponseEntity.ok(HttpStatus.OK);
     }
 }
