fb115464986af1e347ef370154a44d44363ec2a7
Alexey Timofeev <lesha.timofeev2017@yandex.ru>
2023-03-03 01:18:35 +0300

    adding tests for throwing exceptions

--- src/test/java/unit/songsTest/SongServiceTest.java
+++ src/test/java/unit/songsTest/SongServiceTest.java
@@ -1,5 +1,6 @@
 package unit.songsTest;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,19 +10,26 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import ru.tinkoff.tinkoffmusicplatform.data.Song;
+import ru.tinkoff.tinkoffmusicplatform.exception.SongNotFoundException;
 import ru.tinkoff.tinkoffmusicplatform.repository.SongRepository;
 import ru.tinkoff.tinkoffmusicplatform.service.impl.SongServiceImpl;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
+import static ru.tinkoff.tinkoffmusicplatform.constants.ErrorMessageKeeper.*;
 
 @ExtendWith(MockitoExtension.class)
 public class SongServiceTest {
 
+    private static final String NOT_EXISTING_TITLE = "Empty title";
+    private static final String NOT_EXISTING_AUTHOR = "Empty author";
+    private static final String NOT_EXISTING_GENRE = "Empty genre";
+    private static final long NOT_EXISTING_ID = new Random().nextLong(100000000);
     @Mock
     private SongRepository songRepository;
 
@@ -54,6 +62,7 @@ public class SongServiceTest {
                 .author(author)
                 .genre(genre)
                 .build();
+
     }
 
     @Test
@@ -132,5 +141,60 @@ public class SongServiceTest {
         songService.deleteById(1L);
 
         verify(songRepository, times(0)).deleteById(song.getId());
+    }
+
+    @Test
+    public void itShouldCatchSongNotFoundExceptionForFindingById() {
+        when(songRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        SongNotFoundException exception = Assertions.assertThrows(SongNotFoundException.class, () -> {
+            Song song = songService.getSongById(NOT_EXISTING_ID);
+        });
+        verify(songRepository, times(1)).findById(anyLong());
+        assertEquals(SONG_WITH_THIS_ID_NOT_FOUND, exception.getMessage());
+    }
+
+    @Test
+    public void itShouldCatchSongNotFoundExceptionForFindingByTitle() {
+        when(songRepository.findByTitle(NOT_EXISTING_TITLE)).thenReturn(List.of());
+
+        SongNotFoundException exception = Assertions.assertThrows(SongNotFoundException.class, () -> {
+            List<Song> songList = songService.getSongsByTitle(NOT_EXISTING_TITLE);
+        });
+        verify(songRepository, times(1)).findByTitle(NOT_EXISTING_TITLE);
+        assertEquals(SONG_WITH_THIS_TITLE_NOT_FOUND, exception.getMessage());
+    }
+
+    @Test
+    public void itShouldCatchSongNotFoundExceptionForFindingByAuthor() {
+        when(songRepository.findByAuthor(NOT_EXISTING_AUTHOR)).thenReturn(List.of());
+
+        SongNotFoundException exception = Assertions.assertThrows(SongNotFoundException.class, () -> {
+            List<Song> songList = songService.getSongsByAuthor(NOT_EXISTING_AUTHOR);
+        });
+        verify(songRepository, times(1)).findByAuthor(NOT_EXISTING_AUTHOR);
+        assertEquals(SONG_WITH_THIS_AUTHOR_NOT_FOUND, exception.getMessage());
+    }
+
+    @Test
+    public void itShouldCatchSongNotFoundExceptionForFindingByGenre() {
+        when(songRepository.findByGenre(NOT_EXISTING_GENRE)).thenReturn(List.of());
+
+        SongNotFoundException exception = Assertions.assertThrows(SongNotFoundException.class, () -> {
+            List<Song> songList = songService.getSongsByGenre(NOT_EXISTING_GENRE);
+        });
+        verify(songRepository, times(1)).findByGenre(NOT_EXISTING_GENRE);
+        assertEquals(SONG_WITH_THIS_GENRE_NOT_FOUND, exception.getMessage());
+    }
+
+    @Test
+    public void itShouldCatchSongNotFoundExceptionForDeleting() {
+        when(songRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        SongNotFoundException exception = Assertions.assertThrows(SongNotFoundException.class, () -> {
+            songService.deleteById(NOT_EXISTING_ID);
+        });
+        verify(songRepository, times(1)).findById(anyLong());
+        assertEquals(SONG_NOT_DELETED, exception.getMessage());
     }
 }
